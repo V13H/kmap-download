ff75b3faf2387a6971b4288da56b045868bc95ad
Volodymyr Holiev <vovagolev23@gmail.com>
2020-08-03 22:18:22 +0300

    Test commit

--- src/main/java/com/hvs/downloads/kmap/DownloadController.java
+++ src/main/java/com/hvs/downloads/kmap/DownloadController.java
@@ -1,6 +1,5 @@
 package com.hvs.downloads.kmap;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,11 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
 
 @Controller
 @RequestMapping("/download")
@@ -27,15 +22,14 @@ public class DownloadController {
     }
 
     @GetMapping("/k-map")
-    public ResponseEntity<InputStreamResource> downloadKMap() throws URISyntaxException, IOException {
-        URL url = new ClassPathResource("/downloads/" + APP).getURL();
-        File file = new File(getClass().getResource("/downloads/app-debug.apk").toExternalForm());
-        InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
+    public ResponseEntity<InputStreamResource> downloadKMap() {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/downloads/app-debug.apk");
+        InputStreamResource in = new InputStreamResource(resourceAsStream);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + APP
                 )
-                .contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(file.length())
-                .body(inputStreamResource);
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(in);
     }
 
 }
